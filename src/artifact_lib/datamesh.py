396968579329fcf042ae835bc2ba4c29a1c1a1ff
hdelima1993v2 <hernandesls1993@gmail.com>
2025-07-20 15:55:28 -0300

    atualizações

---
+++ src/artifact_lib/datamesh.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import awswrangler as wr
+import pandas as pd
+import os
+
+def ingest_expenses_sor(df: pd.DataFrame, user: str) -> dict:
+    """
+    Função responsável por preencher a tabela despesas_sor de acordo com o 
+    banco de dados que o usuário está executando
+
+    :param df: dataframe com os gastos que serão inseridos na tabela.
+    :param user: usuário que está executando a aplicação.
+    
+    :return: dicionário com a quantidade de linhas inseridas na tabela.
+    """
+    try:
+        database = 'db_source_expenses_' + user
+        table = 'despesas_sor'
+        for field in df.columns:
+            df[field] = df[field].astype(str).replace({'nan': None, 'NaT':None, 
+                'None': None})
+
+        # Escrever (append)
+        wr.s3.to_parquet(
+            df=df, 
+            path=f's3://ddd-dbsource-datamesh/{database}/{table}/',
+            dataset=False,
+            database=database,
+            table=table,
+            mode="append"
+        )
+
+        return {"linhas_gravadas": len(df)}
+    
+    except Exception as e:
+        raise Exception('[datamesh.ingest_expenses_sor] erro ao inserir dados'
+            f' na tabela:{e}')
---
+++ src/artifact_lib/utils.py
@@ -0,0 +1,37 @@
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+
+
+def get_dateref_yyyymm(df: pd.DataFrame, date_column:str,
+                        format_field:str, sell_type: str)-> str:
+    """
+    Identifica a data referência em uma coluna de um DataFrame pandas e retorna
+    o ano e o mês no formato YYYYMM.
+
+    :param df: DataFrame contendo a coluna de datas.
+    :param date_column: Nome da coluna que contém as datas.
+    :param format_field: formato da data que o campo possuí.
+    :param sell_type: tipo de despesa do arquivo.
+    
+    :return str: ano mês min/max no formato YYYYMM.
+    """
+    try:
+        # Converte a coluna para datetime, se ainda não for
+        df[date_column] = pd.to_datetime(
+                            df[date_column], 
+                            format=format_field,
+                            errors='coerce')
+        
+        # Encontra a data máxima e retorna no formato desejado
+        if sell_type == 'debito':
+            dateref = df[date_column].min()
+
+        if sell_type == 'credito':
+            dateref = df[date_column].max()
+            dateref = dateref + relativedelta(months=1)
+
+        return dateref.strftime('%Y%m') if pd.notnull(dateref) else None
+    
+    except Exception as e:
+            raise Exception('[utils - functions.get_dateref_yyyymm] Erro ao '
+                f'capturar o dataref da tabela: (Tipo:{sell_type}, erro:{e})')
