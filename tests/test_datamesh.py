277c30c2a600125339e20d19044edb9f452030ac
hdelima1993v2 <hernandesls1993@gmail.com>
2025-07-22 21:32:25 -0300

    Alteração dos testes

--- tests/test_datamesh.py
+++ tests/test_datamesh.py
@@ -1,12 +1,15 @@
 from artifact_lib.datamesh import ingest_expenses_sor
+from unittest.mock import patch, MagicMock
 import pandas as pd
 import logging
 import pytest
 import time
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+@patch('artifact_lib.datamesh.wr', MagicMock())
 def test_ingest_expenses_sor(mock_dataframe):
     df = pd.DataFrame(mock_dataframe)
     version = int(time.time())
@@ -17,6 +20,7 @@ def test_ingest_expenses_sor(mock_dataframe):
     logger.info(f'teste efetuado! Versão:{version}')
     assert result == {"linhas_gravadas": len(df)}
 
+@patch('artifact_lib.datamesh.wr', MagicMock())
 def test_except_ingest_expenses_sor(mock_dataframe):
     user = "thorfin"
     version = int(time.time())
