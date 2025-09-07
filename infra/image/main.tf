5b01b3f544d14051e0df5ae00a6ec131c208537c
hdelima1993v2 <hernandesls1993@gmail.com>
2025-09-07 20:00:13 -0300

    Alterações

--- infra/image/main.tf
+++ infra/image/main.tf
@@ -1,14 +1,15 @@
-variable "image_tag"      { type = string }                       # passado pela esteira
-
 data "aws_caller_identity" "me" {}
 data "aws_region" "cur" {}
 
-resource "aws_ecr_repository" "lambda" {
-  name = "artifact-fn"
+resource "aws_ecr_repository" "artifact_fn" {
+  name = var.repo_name
+
+  image_scanning_configuration { scan_on_push = true }
+  encryption_configuration     { encryption_type = "AES256" }
 }
 
 resource "aws_lambda_function" "fn" {
-  function_name = "artifact-fn"
+  function_name = var.repo_name
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.lambda.repository_url}:${var.image_tag}"
   role          = aws_iam_role.lambda_exec.arn
---
+++ infra/image/variables.tf
@@ -0,0 +1,8 @@
+variable "image_tag" { 
+    type = string 
+}                      
+
+variable "repo_name" {
+  type    = string
+  default = "artifact-fn"
+}
