e2cc4db6cdac94537256402473888d43e281314d
hdelima1993v2 <hernandesls1993@gmail.com>
2025-09-07 19:45:40 -0300

    teste

--- infra/image/src/app.py
+++ infra/image/src/app.py
@@ -2,4 +2,6 @@ import json, pyarrow as pa
 
 def handler(event, context):
     _ = pa.array([1,2,3])  # sanity
-    return {"statusCode": 200, "body": json.dumps({"ok": True, "engine": "container"})}
+    return {"statusCode": 200, "body": json.dumps({"ok": True, 
+        "engine": "container"})}
+
