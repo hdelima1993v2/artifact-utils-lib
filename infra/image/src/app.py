8b1341626a65b2f09c05d3465ed29fcb63f10677
hdelima1993v2 <hernandesls1993@gmail.com>
2025-09-07 19:39:30 -0300

    Tentativa deploy image

--- infra/image/src/app.py
+++ infra/image/src/app.py
@@ -3,4 +3,3 @@ import json, pyarrow as pa
 def handler(event, context):
     _ = pa.array([1,2,3])  # sanity
     return {"statusCode": 200, "body": json.dumps({"ok": True, "engine": "container"})}
-  
