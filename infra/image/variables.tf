94f9b3cd0c82e4d504dac1e1c15a7e4f756067a2
hdelima1993v2 <hernandesls1993@gmail.com>
2025-09-07 23:54:03 -0300

    ajustes

--- infra/image/variables.tf
+++ infra/image/variables.tf
@@ -1,8 +1,16 @@
 variable "image_tag" { 
-    type = string 
+  description = "Tag da imagem no ECR usada pela Lambda"
+  type        = string
+  default     = ""   # na etapa de criar/garantir o ECR não é usada
 }                      
 
 variable "repo_name" {
   type    = string
   default = "artifact-fn"
+}
+
+variable "lambda_role_arn" {
+  description = "ARN da role de execução da Lambda (opcional na etapa ECR)"
+  type        = string
+  default     = null
 }
